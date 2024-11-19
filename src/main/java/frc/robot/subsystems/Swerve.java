fecd4b2b7b5a80e44778264941f789f3ea8dbe34
Sami Alameddine <sami@als.fyi>
2024-11-18 16:43:08 -0800

    vision

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -5,15 +5,34 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team696.lib.Camera.LimeLightCam;
+import frc.team696.lib.Swerve.SwerveDriveSubsystem;
 
-public class Swerve extends SubsystemBase {
-  /** Creates a new Swerve. */
+public class Swerve extends SwerveDriveSubsystem {
+  public final LimeLightCam shooterCam;
+  public final LimeLightCam noteCam;
+  private static Swerve swerve;
+  public static Swerve get(){
+    if (swerve==null) swerve=new Swerve();
+    return swerve;
+  }
   public Swerve() {
+    shooterCam=new LimeLightCam("limelight-shooter");
+    noteCam=new LimeLightCam("limelight-note");
+
+    shooterCam.addVisionEstimate(this::addVisionMeasurement, (latestResult)->{
+      if(latestResult.ambiguity>0.4||getState().angularVelocity()>2.5){
+        return false;
+      }
+      if(latestResult.pose.getX()<0||latestResult.pose.getX()>8.3||latestResult.pose.getY()<0||latestResult.pose.getY()>16.3)
+        return false;// robot is literally off the field
+      double deviationRatio=(Math.pow((latestResult.distToTag),2)/12);
+      shooterCam.setStdDeviations(deviationRatio/5, deviationRatio/5, deviationRatio/5);
+      return true;
+    });
+  }
+  public void onUpdate(){
     
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
 }
